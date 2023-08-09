5f0d3b69309b5974d311864bb8e306fbd3e3a5e7
teachmycloud <137715661+teachmycloud@users.noreply.github.com>
2023-08-09 23:45:46 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
-sudo yum -y update
-sudo yum install -y httpd php git
-sudo systemctl enable httpd.service
-sudo service httpd start
-cd
-sudo git clone https://github.com/AventureCloud/centos-website-install
-sudo cp ./centos-website-install/* /var/www/html
+
+sudo apt-get update
+sudo apt-get install -y apache2 php git
+sudo systemctl enable apache2.service
+sudo service apache2 start
+sudo git clone https://github.com/teachmycloud/apache-automation
+sudo cp ./apache-automation/* /var/www/html
 sudo mv /var/www/html/htaccess /var/www/html/.htaccess
+sed -i '19iDirectoryIndex index.php /html/index.php' /etc/apache2/sites-available/000-default.conf 
+sudo service apache2 restart
 
