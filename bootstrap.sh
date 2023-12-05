f23f17a387c2ef6ad17b0be9a6f45177636b782f
teachmycloud <137715661+teachmycloud@users.noreply.github.com>
2023-12-05 09:31:32 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ sudo service apache2 start
 sudo git clone https://github.com/teachmycloud/apache-automation
 sudo cp ./apache-automation/* /var/www/html
 sudo mv /var/www/html/htaccess /var/www/html/.htaccess
-sed -i '19iDirectoryIndex index.php /html/index.php' /etc/apache2/sites-available/000-default.conf 
+sudo sed -i '19iDirectoryIndex index.php /html/index.php' /etc/apache2/sites-available/000-default.conf 
 sudo service apache2 restart
 
